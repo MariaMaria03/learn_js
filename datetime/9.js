c177c5ca7f7040bc5121e9c8c966d1536664f789
Maria <fulgentuniverse@gmail.com>
2016-03-07 21:39:15 +0500

    Исправляет последнюю задачу в разделе Время и Дата

--- datetime/9.js
+++ datetime/9.js
@@ -1,24 +1,38 @@
 
 //исправить
 function formatDate(date) {
-    alert(date);
     new_date = new Date();
-    if (new_date.getSeconds() - date.getSeconds() < 1) {
+    difference = new_date - date;
+
+    if (difference/1000 < 1) {
         return "Только что";
     }
-    else if (new_date.getMinutes() - date.getMinutes() < 1) {
-        return new_date.getSeconds() - date.getSeconds() + " сек. назад";
-    }
-    else if (new_date.getHours() - date.getHours() < 1) {
-        return new_date.getMinutes() - date.getMinutes() + " мин. назад";
-    }
-    else {
-        return date;
+
+    var sec = Math.floor(difference/1000);
+    if (sec < 60) {
+        return sec + " сек. назад";
     }
 
+    var min = Math.floor(difference/60000);
+    if (min < 60) {
+        return min + " мин. назад";
+    }
+
+    var str_year = (date.getYear() + '').slice(-2);
+    var str_day =  date.getDate();
+    if (str_day < 10) {
+        str_day = '0' + str_day;
+    }
+    var str_month = date.getMonth() + 1;
+    if (str_month < 10) {
+        str_month = '0' + str_month;
+    }
+    var str_date = str_day + '.' + str_month + '.' + str_year +
+        ' ' + date.getHours() + ':' + date.getMinutes();
+    return str_date
 }
 
 alert(formatDate(new Date(new Date - 1)));
-alert( formatDate(new Date(new Date - 35 * 1000)) );
-alert( formatDate(new Date(new Date - 14 * 60 * 1000)) );
-alert( formatDate(new Date(new Date - 86406 * 1000)) );
+alert(formatDate(new Date(new Date - 34 * 1000)));
+alert(formatDate(new Date(new Date - 12 * 60 * 1000)));
+alert(formatDate(new Date(new Date - 86400 * 1000)));
